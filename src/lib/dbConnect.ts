ae9db22dde49a8d1f65f0e2cb4cc26f89fca7712
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-17 17:38:43 +0530

    db problem solve

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -14,8 +14,6 @@ async function connentDb():Promise<void> {
 
     try {
         const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || '', {})
-        // console.log('mongoose connection data', db)
-
         connection.isConnected = db.connections[0].readyState
 
         // console.log("ready state:",db.connections[0].readyState)
