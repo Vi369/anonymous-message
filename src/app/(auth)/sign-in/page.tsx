82ecafd8d7b108cf12cd27f72aa8e4457909c6da
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-24 16:25:46 +0530

    shadcn install and form added

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+function page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
