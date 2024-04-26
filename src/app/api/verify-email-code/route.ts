02e3bfb2334aa1a33eea5cf5a9af757554047800
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-26 14:52:36 +0530

    some modification in email verify api route

--- src/app/api/verify-email-code/route.ts
+++ src/app/api/verify-email-code/route.ts
@@ -5,29 +5,31 @@ import { verifySchema } from '@/schemas/verifySchema'
 import { ResponseObj } from "@/helpers/ResponseObj";
 
 // check email code schema 
-const verifyEmailCodeSchema = z.object({
-    code: verifySchema
-})
+// const verifyEmailCodeSchema = z.object({
+//     code: verifySchema
+// })
 
 export async function POST(request:Request) {
     await connentDb()
 
     try {
         const {username, code} = await request.json()
+        console.log("username and code", username,code)
         // const decodedUsername = decodeURIComponent(username)
+        // console.log(decodedUsername)
 
-        // code validation
-        const result = verifyEmailCodeSchema.safeParse({code});
+        // // code validation
+        // const result = verifyEmailCodeSchema.safeParse({code});
 
-        if(!result.success){
-            return Response.json({
-                success: false,
-                message: result.error.format().code?._errors
-            },{status: 400})
-        }
+        // if(!result.success){
+        //     return Response.json({
+        //         success: false,
+        //         message: result.error.format().code?._errors
+        //     },{status: 400})
+        // }
 
-        // validated Code 
-        const validatedCode = result.data.code
+        // // validated Code 
+        // const validatedCode = result.data.code.code
 
         const user = await UserModel.findOne({
             username
@@ -40,7 +42,7 @@ export async function POST(request:Request) {
             })
         }
 
-        const isCodevalid = user.verifiedCode === validatedCode;
+        const isCodevalid = user.verifiedCode === code;
 
         if(isCodevalid){
             return Response.json( 
@@ -55,7 +57,7 @@ export async function POST(request:Request) {
             user.isVerified = true
             await user.save()
             return Response.json(
-                new ResponseObj(true, "User Email Verified Successfully!"),
+                new ResponseObj(true, "Email Verified Successfully!"),
                 {status: 200}
             )
         }else{
