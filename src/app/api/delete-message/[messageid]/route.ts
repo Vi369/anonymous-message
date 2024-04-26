e8340155ff1fb763f71c08b4fb8910cc4316ac89
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-26 21:04:41 +0530

    delete message api route successfully completed

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,45 @@
+import { ResponseObj } from "@/helpers/ResponseObj";
+import UserModel from "@/model/user.model";
+import { User, getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function DELETE(request:Request,{params}:{params:{messageid:string}}) {
+    const messageId = params.messageid;
+    try {
+        const session = await getServerSession(authOptions);
+        const user:User = session?.user
+        // TODO: console session or user to check details 
+        console.log("session or user found or not:", session, user)
+        // checking if session expire 
+        if(!session && !user){
+            return Response.json(
+                new ResponseObj(false, "Authentication failed: User session has expired or user is not authenticated."),
+                {status: 401}
+            )
+        }
+        const updatedUserMessagesArray = await UserModel.updateOne(
+            {_id: user._id},
+            {$pull:{_id: messageId}}
+        )
+        // TODO: console 
+        console.log("delete message response", updatedUserMessagesArray)
+        if(updatedUserMessagesArray.modifiedCount===0){
+            return Response.json(
+                new ResponseObj(false,"Message not Found or Alreday deleted!"),
+                {status: 404}
+            )
+        }
+        // success response
+        return Response.json(
+            new ResponseObj(true, "Message deleted Successfully!!"),
+            {status: 200}
+        )
+
+    } catch (error) {
+        console.log("Error During deleting message:", error)
+        return Response.json(
+            new ResponseObj(false, "Error Occured while deleting the message"),
+            {status: 500}
+        )
+    }
+}
