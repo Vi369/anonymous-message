3c347145a46482e8621a1e68fd2d777348b99384
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-27 20:51:21 +0530

    navbar component added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { User } from 'next-auth'
+import { signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+function Navbar() {
+    const {data:session} = useSession()
+    const user:User = session?.user
+    return (
+        <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <Link href={'/'} className="text-xl font-bold mb-4 md:mb-0">
+                    Anonymous Feedback
+                </Link>
+                {
+                    session? (<>
+                        <span className='mr-4'>Welcome{user?.username || user?.email}</span>
+                        {/* logout button */}
+                        <Button onClick={()=>signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant={'ghost'}>Sign-out</Button>
+                        </>):(<>
+                        <Link href={'/sign-in'}>
+                            <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'ghost'}>Sign-in</Button>
+                        </Link>
+                    </>)
+                }
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
