08f2dbb3973db5704619bdb26ec96d51e9c06a63
Vikas singh varma <Vikashverma3001@gmail.com>
2024-04-27 14:52:48 +0530

    password validation added in signin schema

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -2,5 +2,5 @@ import {z} from 'zod'
 
 export const signInSchema = z.object({
     identifire: z.string(),
-    password: z.string()
+    password: z.string().min(8,{message: 'password must be 8 charcters'})
 })
