04b00c962d6655a5ea5c042fd284862f188f8345
Vikas singh varma <Vikashverma3001@gmail.com>
2024-05-02 20:09:52 +0530

    error solve

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,7 +1,7 @@
 import {z} from 'zod'
 
-export const acceptMessageSchema = z.object({
-    Content: z
+export const MessageSchema = z.object({
+    content: z
         .string()
         .min(10, {message: 'Content must be 10 characters'})
         .max(300, {message: 'Content must be no more longer than 300 characters'})
